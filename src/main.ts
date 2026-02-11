47343542dcb71efbdb6840104e7c0bddc532b4f2
Marco Túlio <marcotulio_iub@hotmail.com>
2026-02-11 09:33:31 -0300

    implement validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+  }));
   const config = new DocumentBuilder()
     .setTitle('API de Carteira Financeira')
     .setDescription('Desafio Backend NODE.JS - Sistema de depósitos e transferências')
